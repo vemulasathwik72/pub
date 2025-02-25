8bd9bc7d50f644e53027ca549856f56b9ae62453
vemulasathwik72@gmail.com <vemulasathwik72@gmail.com>
2025-02-25 14:56:44 +0530

    new feature2.2 added by sathwik

--- Application.java
+++ Application.java
@@ -8,6 +8,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+#feature2.2 changes added by sathwik
+export default function App() 
+  return (
+    <View>
+          <Home/>
+    </View>
+  );
+       
 </head>
 
 <body>
